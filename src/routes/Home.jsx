452e4cc60b4400623e89ad626533a4fc709859cb
Aloice Ogana <aloiceadriot@gmail.com>
2024-05-08 01:40:58 +0300

    import.home-exp

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,20 @@
+import ExploreMenu from "../components/ExploreMenu/ExploreMenu";
 import Navbar from "../components/Navbar";
 
-
-
 function Home() {
-  return <div>
-    <Navbar />
-    HOOOOOOOOOOOOOME</div>;
+  return (
+    <div>
+      <Navbar />
+      HOOOOOOOOOOOOOME
+    </div>
+  );
 }
+const Home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
 
 export default Home;
