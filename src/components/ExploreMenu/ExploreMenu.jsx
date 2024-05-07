9aae6b71e8af31c75f3d6026033c7a1919648ebb
Aloice Ogana <aloiceadriot@gmail.com>
2024-05-08 01:04:39 +0300

    imports

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../assets/assets";
 
 function ExploreMenu() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
 
-export default ExploreMenu
+export default ExploreMenu;
