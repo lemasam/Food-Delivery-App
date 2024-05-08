4b0dba19057fc505fc7bac964765ed8e25037f53
joe <brakejoe9@gmail.com>
2024-05-08 15:50:27 +0300

    "item1"

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+// FoodItem.js
+import React from 'react';
 
-function FoodItem() {
+const FoodItem = ({ name, description, price, image }) => {
   return (
-    <div>
-      
+    <div className="food-item">
+      <img src={image} alt={name} />
+      <div className="food-details">
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <p>Price: ${price}</p>
+        <button>Add to Cart</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default FoodItem
+export default FoodItem;
