649b37b3b211c50d829b2db288a201f08ad03f88
Aloice Ogana <aloiceadriot@gmail.com>
2024-05-08 20:52:06 +0300

    nav.bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
+import React from "react";
+import Navbar from "./components/NavBar/Navbar";
 
-
-function App() {
-  
-
+const App = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <div className="app">
+      <Navbar></Navbar>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,12 +1,28 @@
-import { NavLink } from "react-router-dom"
+import React from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav>
-      <NavLink to={"/"}>Home</NavLink>
-      <NavLink to={"/about"}>About </NavLink>
-    </nav>
+    <div className="nav3bar">
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
+      <div className="navgar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot">
+            <button>sign in</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
