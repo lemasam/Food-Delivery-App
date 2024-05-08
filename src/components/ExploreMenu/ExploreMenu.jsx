65b467a6188ccac8c213d5baa662f858947ac5c4
Aloice Ogana <aloiceadriot@gmail.com>
2024-05-08 15:23:06 +0300

    import.home-exp

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -9,3 +9,11 @@ const ExploreMenu = () => {
       <p className="explore-menu-text">
         choose from a diverse menu featuring a delicatable array of dishes. Our
         mission is to satisfy{" "}
+        </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div key={index} className="explore-menu-list-item">
+              <img src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
