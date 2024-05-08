c95ca8572391fe6b6d15d3c49942ceda61deb77f
Aloice Ogana <aloiceadriot@gmail.com>
2024-05-09 02:57:07 +0300

    padding

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../assets/assets";
 
-function ExploreMenu() {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
-    <div>
-      
+    <div className="explore-menu" id="explore-menu">
+      <h1>Exolore our menu</h1>
+      <p className="explore-menu-text">
+        choose from a diverse menu featuring a delicatable array of dishes. Our
+        mission is to satisfy{" "}
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() => (prev) =>
+                prev === item.menu_name ? "ALL" : item.menu_name}
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
     </div>
-  )
-}
-
-export default ExploreMenu
+  );
+};
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import ",/Home.css";
+import Header from "../../components/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+
+const Home = () => {
+  t;
+
+  const [category, setCategory] = useState("ALL");
+
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+    </div>
+  );
+};
+
+export default Home;
