1a0eac9dd7fc4d4fd425fcf753602d5b691476ac
Immaculatewangari <immaculate.wangari@student.moringaschool.com>
2024-05-09 10:07:46 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 
-
-function App() {
-  
-
+const App = () => {
   return (
     <>
-      
+      <div className='app'>
+        <Navbar />
+        <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
+    </div >
+      <Footer />
     </>
+    
   )
 }
+
 
 export default App
