35be985cf85f5baa742931705dcd3efbadb4b284
Aloice Ogana <aloiceadriot@gmail.com>
2024-05-09 16:07:15 +0300

    all

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ExploreMenu.css";
-import { menu_list } from "../assets/assets";
+import { StoreContext } from "../../Context/StoreContext";
 
 const ExploreMenu = ({ category, setCategory }) => {
+  const { menu_list } = useContext(StoreContext);
+
   return (
     <div className="explore-menu" id="explore-menu">
-      <h1>Exolore our menu</h1>
+      <h1>Explore our menu</h1>
       <p className="explore-menu-text">
-        choose from a diverse menu featuring a delicatable array of dishes. Our
-        mission is to satisfy{" "}
+        Browse our varied menu with tasty dishes. We aim to make you happy and
+        improve your meal experience, one delicious dish at a time..
       </p>
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
             <div
-              onClick={() => (prev) =>
-                prev === item.menu_name ? "ALL" : item.menu_name}
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
               key={index}
               className="explore-menu-list-item"
             >
               <img
-                className={category === item.menu_name ? "active" : ""}
                 src={item.menu_image}
+                className={category === item.menu_name ? "active" : ""}
                 alt=""
               />
               <p>{item.menu_name}</p>
@@ -33,3 +38,5 @@ const ExploreMenu = ({ category, setCategory }) => {
     </div>
   );
 };
+
+export default ExploreMenu;
---
+++ src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function FoodItem() {
+  const { id } = useParams();
+  const [foodItem, setFoodItem] = useState(null);
+
+  useEffect(() => {
+    fetchFoodItem();
+  }, []);
+
+  const fetchFoodItem = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/foodItems/${id}`);
+      const data = await response.json();
+      setFoodItem(data);
+    } catch (error) {
+      console.error("Error fetching food item:", error);
+    }
+  };
+
+  return (
+    <div>
+      {foodItem ? (
+        <div>
+          <h2>{foodItem.name}</h2>
+          <p>Description: {foodItem.description}</p>
+          <p>Price: ${foodItem.price}</p>
+          <img
+            src={`http://localhost:3000/${foodItem.image}`}
+            alt={foodItem.name}
+            style={{ maxWidth: "100%" }}
+          />
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default FoodItem;
