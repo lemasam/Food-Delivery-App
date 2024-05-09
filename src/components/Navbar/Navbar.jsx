9f7ab57fe07c18f16c4e045be31696b04252ea1a
Aloice Ogana <aloiceadriot@gmail.com>
2024-05-09 19:51:22 +0300

    all

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,33 @@
-import { NavLink } from "react-router-dom"
+import React, { useContext, useState } from 'react'
+import  './Navbar.css'
+import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
+import { StoreContext } from '../../Context/StoreContext'
 
+const Navbar = ({setShowLogin}) => {
 
+  const [menu,setMenu] = useState("home");
+  const {getTotalCartAmount} = useContext(StoreContext);
 
-function Navbar() {
   return (
-    <nav className=" rounded-full text-2xl font-bold md:mt-6 md:mx-48 bg-slate-500 ">
-      <NavLink to={"/"} className="p-48 w-full">
-        Home:
-      </NavLink>
-      <NavLink to={"/about"} className="p-48 w-full">
-        About:
-      </NavLink>
-      <NavLink to={"/contactus"} className=" p-48 w-full">
-        Contact Us:
-      </NavLink>
-    </nav>
-  );
+    <div className='navbar'>
+      <Link to='/'><img className='logo' src={assets.logo} alt="" /></Link>
+      <ul className="navbar-menu">
+        <Link to="/" onClick={()=>setMenu("home")} className={`${menu==="home"?"active":""}`}>home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={`${menu==="menu"?"active":""}`}>menu</a>
+        <a href='#app-download' onClick={()=>setMenu("mob-app")} className={`${menu==="mob-app"?"active":""}`}>mobile app</a>
+        <a href='#footer' onClick={()=>setMenu("contact")} className={`${menu==="contact"?"active":""}`}>contact us</a>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <Link to='/cart' className='navbar-search-icon'>
+          <img src={assets.basket_icon} alt="" />
+          <div className={getTotalCartAmount()>0?"dot":""}></div>
+        </Link>
+        <button onClick={()=>setShowLogin(true)}>sign in</button>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
